d7fc895a9ecd716992ce332963b58c795c9428d6
wellssd <scott.wells@tsmidwest.com>
2019-12-26 23:10:42 -0500

    Add Status Bar
    Add Status Bar to syncrodash.php that includes a proportional graphical display of ticket status based on due date.  Status bar can be enabled or disabled, and thresholds for status set in hours.  The included cutoff parameters indicate the minimum number of hours until a ticket is due to meet the category.  IE...by default, green (which aligns to "Available" in Zabbix) tickets have at least 48 hours until due.

--- syncrodash.php
+++ syncrodash.php
@@ -9,6 +9,7 @@
 # Date     Notes
 # -------- ------------------------------------------------------------------------------------------------------------
 # 20191223 Initial version (software@tsmidwest.com)
+# 20191226 Add status bar with colors aligned to Zabbix dark theme (software@tsmidwest.com)
 #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
@@ -16,8 +17,12 @@
 ###
 ### Definitions
 ###
-$API_KEY   = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"; # Syncro key to access REST API
-$SUBDOMAIN = "xxxxxxxxx";                            # Syncro subdomaing
+$API_KEY    = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"; # Syncro key to access REST API
+$SUBDOMAIN  = "xxxxxxxxx";                            # Syncro subdomaing
+$STATUS_BAR = 1;                                      # Display status bar (0 = no, 1 = yes)
+$STATUS_AVL = 48;                                     # Cutoff in hours for tickets to be "Available" (green) status
+$STATUS_WRN = 24;                                     # Cutoff in hours for tickets to be "Warn" (yellow) status
+$STATUS_HGH = 0;                                      # Cutoff in hours for tickets to be "High" (orange) status
 
 
 ###
@@ -44,7 +49,7 @@ print "    td, th {\n";
 print "        padding: 5px;\n";
 print "    }\n";
 print "</style>\n\n";
-print "<body>\n";
+print "<body>\n\n";
 
 
 ###
@@ -52,6 +57,42 @@ print "<body>\n";
 ###
 $URI = "https://".$SUBDOMAIN.".syncromsp.com/api/v1/tickets?api_key=".$API_KEY."&status=Not%20Closed";
 $Tickets = json_decode(file_get_contents($URI));
+
+
+###
+### Status Bar - If enabled, use an HTML table to create a status bar
+###
+if($STATUS_BAR == 1) {
+        if(count($Tickets->tickets) >= 1) {
+                $tix_avl = 0; # Counter for "Available" tickets
+                $tix_wrn = 0; # Counter for "Warn" tickets
+                $tix_hgh = 0; # Counter for "High" tickets
+                $tix_dsr = 0; # Counter for "Disaster" tickets
+                foreach($Tickets->tickets as $ticket) {
+                        $duedate = strtotime($ticket->due_date);
+                        if($duedate >= (time()+($STATUS_AVL*3600))) {
+                                $tix_avl++;
+                        } elseif($duedate >= (time()+($STATUS_WRN*3600))) {
+                                $tix_wrn++;
+                        } elseif($duedate >= (time()+($STATUS_HGH*3600))) {
+                                $tix_hgh++;
+                        } else {
+                                $tix_dsr++;
+                        }
+                }
+                $avltix = ceil((100/($tix_avl+$tix_wrn+$tix_hgh+$tix_dsr))*$tix_avl);
+                $wrntix = ceil((100/($tix_avl+$tix_wrn+$tix_hgh+$tix_dsr))*$tix_wrn);
+                $hghtix = ceil((100/($tix_avl+$tix_wrn+$tix_hgh+$tix_dsr))*$tix_hgh);
+                $dsrtix = ceil((100/($tix_avl+$tix_wrn+$tix_hgh+$tix_dsr))*$tix_dsr);
+                print "<center>".($tix_avl+$tix_wrn+$tix_hgh+$tix_dsr)." unresolved ticket(s)</center>\n";
+                print "<svg viewbox=\"0 0 ".($tix_avl+$tix_wrn+$tix_hgh+$tix_dsr)." 1\" width=\"100%\" height=\"20\" preserveaspectratio=\"none\">\n";
+                print "    <rect x=\"0\" y=\"0\" width=\"".$tix_avl."\" height=\"1\" style=\"fill:rgb(134, 204, 137);\"></rect>\n";
+                print "    <rect x=\"".$tix_avl."\" y=\"0\" width=\"".$tix_wrn."\" height=\"1\" style=\"fill:rgb(255, 200, 089);\"></rect>\n";
+                print "    <rect x=\"".($tix_avl+$tix_wrn)."\" y=\"0\" width=\"".$tix_hgh."\" height=\"1\" style=\"fill:rgb(233, 118, 089);\"></rect>\n";
+                print "    <rect x=\"".($tix_avl+$tix_wrn+$tix_hgh)."\" y=\"0\" width=\"".$tix_dsr."\" height=\"1\" style=\"fill:rgb(228, 089, 089);\"></rect>\n";
+                print "</svg>\n\n";
+        }
+}
 
 
 ###
